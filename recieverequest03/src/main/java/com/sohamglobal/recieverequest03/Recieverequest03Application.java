f7fcad8587ee66337f33de70b58999a519d83aae
BHAVESH NANDKUMAR LAHOLE <149697798+bhaveshlahole@users.noreply.github.com>
2026-01-31 11:00:13 +0530

    Add files via upload

---
+++ recieverequest03/src/main/java/com/sohamglobal/recieverequest03/Recieverequest03Application.java
@@ -0,0 +1,14 @@
+package com.sohamglobal.recieverequest03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recieverequest03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Recieverequest03Application.class, args);
+		System.out.println("Request poject is running ");
+	} 
+
+}
---
+++ recieverequest03/src/test/java/com/sohamglobal/recieverequest03/Recieverequest03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohamglobal.recieverequest03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recieverequest03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
